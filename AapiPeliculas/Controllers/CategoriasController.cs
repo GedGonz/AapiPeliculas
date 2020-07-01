705111184298520cbe6fcb7a98871b201c5d1075
Gerald Gonzalez <gedgonz7@gmail.com>
2020-06-30 21:33:38 -0600

    Creando el primero ApiController de categorias

---
+++ AapiPeliculas/Controllers/CategoriasController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AapiPeliculas.Repositorios.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AapiPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ICategoriaRepositorio _categoriaRepositorio;
+        public CategoriasController(ICategoriaRepositorio _categoriaRepositorio)
+        {
+            this._categoriaRepositorio = _categoriaRepositorio;
+        }
+
+
+
+    }
+}
