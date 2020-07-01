512941ac6a8088a382b7c9439c02f7d0fd472a28
Gerald Gonzalez <gedgonz7@gmail.com>
2020-06-30 20:30:37 -0600

    Creando IRepositorio Categoria

---
+++ AapiPeliculas/Repositorios/IRepository/ICategoriaRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using AapiPeliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AapiPeliculas.Repositorios.IRepository
+{
+    public interface ICategoriaRepositorio
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int IdCategoria);
+        bool ExistCategoria(string nombre);
+        bool ExistCategoria(int IdCategoria);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool EliminarCategoria(int IdCategoria);
+        bool Guardar();
+    }
+}
