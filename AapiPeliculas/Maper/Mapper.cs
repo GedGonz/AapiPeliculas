96fa9b7f7bc07137a66f75efedd3ec4094321c82
Gerald Gonzalez <gedgonz7@gmail.com>
2020-06-30 21:20:50 -0600

    Agregando AutoMapper y Creando un Profile del AutoMapper

---
+++ AapiPeliculas/Maper/Mapper.cs
@@ -0,0 +1,18 @@
+﻿using AapiPeliculas.Models;
+using AapiPeliculas.Models.DTOS;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AapiPeliculas.Maper
+{
+    public class Mapper:Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Categoria, Categoriadto>().ReverseMap();
+        }
+    }
+}
