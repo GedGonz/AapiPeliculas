962807ccfda20007016b21d34299a2a6b0fcc849
Gerald Gonzalez <gedgonz7@gmail.com>
2020-07-01 19:27:45 -0600

    Creando DTO para Pelicula

---
+++ AapiPeliculas/Models/DTOS/PeliculaCreatedto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static AapiPeliculas.Models.Pelicula;
+
+namespace AapiPeliculas.Models.DTOS
+{
+    public class PeliculaCreatedto
+    {
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La Rutaimagen es requerido")]
+        public string RutaImagen { get; set; }
+        public IFormFile Foto { get; set; }
+        [Required(ErrorMessage = "La Descripcion es requerido")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "La Duracion es requerido")]
+        public string Duracion { get; set; }
+        public TipoClasificacion Clasificacion { get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
---
+++ AapiPeliculas/Models/DTOS/Peliculadto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static AapiPeliculas.Models.Pelicula;
+
+namespace AapiPeliculas.Models.DTOS
+{
+    public class Peliculadto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La Rutaimagen es requerido")]
+        public string RutaImagen { get; set; }
+        [Required(ErrorMessage = "La Descripcion es requerido")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "La Duracion es requerido")]
+        public string Duracion { get; set; }
+        public TipoClasificacion Clasificacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int IdCategoria { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
