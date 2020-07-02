9a9cd4aa7c8d4a6a0f7e06a2aef229bbc549b473
Gerald Gonzalez <gedgonz7@gmail.com>
2020-07-01 19:38:20 -0600

    Creando interface Pelicula

---
+++ AapiPeliculas/Repositorios/IRepository/PeliculaRepositoriocs.cs
@@ -0,0 +1,22 @@
+﻿using AapiPeliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AapiPeliculas.Repositorios.IRepository
+{
+    public interface PeliculaRepositoriocs
+    {
+        ICollection<Pelicula> GetPeliculas();
+        ICollection<Pelicula> GetPeliculasEnCategoria(int IdCategoria);
+        Pelicula GetPelicula(int IdPelicula);
+        bool ExistPelicula(string nombre);
+        IEnumerable<Pelicula> BuscarPelicula(string nombre);
+        bool ExistPelicula(int IdPelicula);
+        bool CrearPelicula(Pelicula pelicula);
+        bool ActualizarPelicula(Pelicula pelicula);
+        bool EliminarPelicula(Pelicula pelicula);
+        bool Guardar();
+    }
+}
