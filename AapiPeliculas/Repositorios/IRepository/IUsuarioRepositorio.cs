55ae2b4887273657028c1a55524458e3f82905f2
Gerald Gonzalez <gedgonz7@gmail.com>
2020-07-02 21:51:57 -0600

    Creando Interface Repositorio e implementandolo

---
+++ AapiPeliculas/Repositorios/IRepository/IUsuarioRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using AapiPeliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AapiPeliculas.Repositorios.IRepository
+{
+    public interface IUsuarioRepositorio
+    {
+        ICollection<Usuario> GetUsuarios();
+        Usuario GetUsuario(int IdUsuario);
+        bool ExistUsuario(string usuario);
+        Usuario Registro(Usuario usuario, string password);
+        Usuario Login(string usuario, string password);
+        bool Guardar();
+    }
+}
---
+++ AapiPeliculas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,93 @@
+﻿using AapiPeliculas.Data;
+using AapiPeliculas.Models;
+using AapiPeliculas.Repositorios.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AapiPeliculas.Repositorios
+{
+    public class UsuarioRepositorio: IUsuarioRepositorio
+    {
+        private readonly AplicationDbContext _db;
+        public UsuarioRepositorio(AplicationDbContext _db)
+        {
+            this._db = _db;
+        }
+
+        public bool ExistUsuario(string usuario)
+        {
+            bool valor = false;
+            try
+            {
+                valor = _db.Usuario.Any(c => c.UsuarioA.ToLower().Trim() == usuario.ToLower().Trim());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return valor;
+        }
+
+        public Usuario GetUsuario(int IdUsuario)
+        {
+            var usuario = new Usuario();
+            try
+            {
+                usuario = _db.Usuario.FirstOrDefault(x=>x.Id==IdUsuario);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return usuario;
+        }
+
+        public ICollection<Usuario> GetUsuarios()
+        {
+            var usuarios = new List<Usuario>();
+            try
+            {
+                usuarios = _db.Usuario.OrderBy(c=>c.UsuarioA).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return usuarios;
+        }
+
+        public bool Guardar()
+        {
+            bool retorno;
+            try
+            {
+                retorno = _db.SaveChanges() >= 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return retorno;
+        }
+
+        public Usuario Login(string usuario, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Registro(Usuario usuario, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
