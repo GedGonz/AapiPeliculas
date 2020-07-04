586c1e3fc0c163539ca59ca82ac63d6f4eff9e82
Gerald Gonzalez <gedgonz7@gmail.com>
2020-07-03 22:25:06 -0600

    Creando controlador Usuario y definiendo los metodos de obntener usuario

---
+++ AapiPeliculas/Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AapiPeliculas.Models.DTOS;
+using AapiPeliculas.Repositorios.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AapiPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly IMapper mapper;
+        public UsuarioController(IUsuarioRepositorio _usuarioRepositorio, IMapper mapper)
+        {
+            this._usuarioRepositorio = _usuarioRepositorio;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsuarios()
+        {
+            var listaUsuarios = _usuarioRepositorio.GetUsuarios();
+
+            var listaUsuariosdto = mapper.Map<List<Usuariodto>>(listaUsuarios);
+
+            return Ok(listaUsuariosdto);
+        }
+
+        [HttpGet("{IdUsuario:int}", Name = "GetUsuario")]
+        public IActionResult GetUsuario(int IdUsuario)
+        {
+            var usuario = _usuarioRepositorio.GetUsuario(IdUsuario);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioDTO = mapper.Map<Usuariodto>(usuario);
+
+            return Ok(usuarioDTO);
+        }
+
+    }
+}
